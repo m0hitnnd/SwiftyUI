b27ab094b84b39b50dde6c22365cc67d6cf64ef8
Mohit <mohit@PMLBLRMB4000047.local>
2020-11-16 18:43:39 +0530

    SwiftUI Essentials project

---
+++ SwiftUIEssentials/SwiftUIEssentials/SwiftUIEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentialsApp.swift
+//  SwiftUIEssentials
+//
+//  Created by Mohit on 16/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
